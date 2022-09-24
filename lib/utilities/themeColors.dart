a0e582544ae5a914da54ca92936484b6e4dca44d
Jocelyn Velarde <a01285780@itesm.mx>
2022-09-24 18:08:44 -0500

    dark design and changed colors

--- lib/utilities/themeColors.dart
+++ lib/utilities/themeColors.dart
@@ -4,4 +4,6 @@ class ThemeColors {
   static Color lightGrey = Color(0xffE8E8E9);
   static Color black = Color(0xff14121E);
   static Color grey = Color(0xFF8492A2);
+  static Color lightRed = Color.fromARGB(0, 255, 104, 104);
+  static Color white = Colors.white;
 }
