bb9ec8ea7af3fc017eb80fdbc4b0a8f57a7a6d3c
Oskar Abraham Sandoval Garcia <44094381+osabga@users.noreply.github.com>
2022-09-25 02:51:27 -0500

    Update settings_screen.dart

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -12,18 +12,25 @@ class _SettingsScreenState extends State<SettingsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      
+appBar: PreferredSize( //wrap with PreferredSize
+                preferredSize: Size.fromHeight(100.0), //height of appbar
+                child: AppBar(
+                  title:Text("Ajustes ",style: TextStyle(fontSize: 50),), //appbar title
+                  backgroundColor: Colors.redAccent, //appbar background color
+                  
+                )
+          ),
+      
+      
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const Text(
-                "Ajustes",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 60, fontWeight: FontWeight.bold),
-              ),
+             
+
               const SizedBox(height: 40),
               SettingsTile(
                 icon: Icons.account_box_rounded,
@@ -59,7 +66,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
               ),
               SettingsTile(
                 icon: Icons.logout_rounded,
-                title: "Cerran Sesion",
+                title: "Cerrar Sesion",
                 onTap: () {},
               ),
             ],
